7a9b16d2feb66b25c2f61dd7c94391e1c3ce75e4
lishengzxc <eric@lishengcn.cn>
2016-10-06 13:00:31 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <vue-quill btn-name="提交"></vue-quill>
+  </div>
+</template>
+
+<script>
+import VueQuill from './VueQuill.vue'
+
+export default {
+  components: {
+    VueQuill
+  }
+}
+</script>
---
+++ src/VueQuill.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="vue-quill">
+    <div id="editor-container">
+
+    </div>
+    <button class="vue-quill-btn" v-on:click="getContents">{{btnName}}</button>
+  </div>
+</template>
+
+<script>
+import Quill from 'quill'
+import style from '../node_modules/quill/dist/quill.snow.css'
+
+export default {
+  mounted() {
+    let toolbar = [
+      [{ 'header': [1, 2, 3, false]}],
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block', 'image', 'link'],
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    ]
+
+    let config = {
+      modules: { toolbar },
+      theme: 'snow'
+    }
+
+    this.quill = new Quill('#editor-container', config)
+  },
+  methods: {
+    getContents: function() {
+      return this.quill.getContents()
+    },
+
+    setContents: function() {
+      return this.quill.setContents.apply(this.quill, agreements)
+    }
+  },
+  props: {
+    height: Number,
+    btnName: String
+  }
+}
+</script>
+
+<style>
+#vue-quill {}
+</style>
